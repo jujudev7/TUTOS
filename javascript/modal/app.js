34abf6a9d7df5e9b3f37a40a8e3fb56f46cf8815
Julien <penna.julien@gmail.com>
2023-12-23 12:40:40 +0100

    comment added in app.js

--- javascript/modal/app.js
+++ javascript/modal/app.js
@@ -6,3 +6,7 @@ modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
 function toggleModal(){
     modalContainer.classList.toggle("active")
 }
+
+// 1. DOM : const selector
+// 2. addEventListener
+// 3. function toggle (show/hide)
